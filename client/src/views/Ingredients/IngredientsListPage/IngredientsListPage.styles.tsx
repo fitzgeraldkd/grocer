545e6105b00d0e4907ac0e327da80af9e20371e7
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-01-03 11:23:40 -0500

    Add pagination to ingredients list page

--- client/src/views/Ingredients/IngredientsListPage/IngredientsListPage.styles.tsx
+++ client/src/views/Ingredients/IngredientsListPage/IngredientsListPage.styles.tsx
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export default styled.div``;
+export default styled.div`
+  .pagination {
+    margin: 20px 0;
+    input {
+
+    }
+
+    button {
+      margin: 0 5px;
+
+      svg ~ svg {
+        margin-left: -8px;
+      }
+    }
+  }
+`;
--- client/src/views/Ingredients/IngredientsListPage/IngredientsListPage.tsx
+++ client/src/views/Ingredients/IngredientsListPage/IngredientsListPage.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import FloatingButton from '../../../components/navigation/FloatingButton/FloatingButton';
 import { RootState } from '../../../rootReducer';
 import IngredientCard from '../IngredientCard/IngredientCard';
 import { RiAddFill } from 'react-icons/ri';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import IngredientsListPageStyles from './IngredientsListPage.styles';
 import { sorter } from '../../../utils/helpers/arrays.helpers';
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
+import Button from '../../../components/forms/Button/Button';
+import Input from '../../../components/forms/Input/Input';
 
 function IngredientsListPage() {
   const navigate = useNavigate();
   const ingredients = useSelector((state: RootState) => state.ingredients.ingredients);
+  const [ingredientPage, setIngredientPage] = useState(1);
+  const [searchParams] = useSearchParams();
+
+  const resultsPerPage = 20;
+
+  console.log(ingredientPage);
+  useEffect(() => {
+    const newPage = parseInt(searchParams.get('page')!, 10);
+    if (!newPage || isNaN(newPage)) setIngredientPage(1);
+    else setIngredientPage(newPage);
+  }, [searchParams])
 
   const handleNewIngredient = () => navigate('/ingredients/new');
+
+  const filteredIngredients = [...ingredients].sort((a, b) => sorter(a.name, b.name));
+  const lastPage = Math.ceil(filteredIngredients.length / resultsPerPage);
 
   return (
     <IngredientsListPageStyles>
       <div className='page-header'>Saved Ingredients</div>
-      {[...ingredients].sort((a, b) => sorter(a.name, b.name)).map(ingredient => <IngredientCard key={ingredient.name} ingredient={ingredient} />)}
+      <div className='pagination'>
+        <Button onClick={() => setIngredientPage(1)}><RiArrowLeftSLine /><RiArrowLeftSLine /></Button>
+        <Button onClick={() => setIngredientPage(Math.max(ingredientPage - 1, 1))}><RiArrowLeftSLine /></Button>
+        <Input value={ingredientPage} />
+        <Button onClick={() => setIngredientPage(Math.min(ingredientPage + 1, lastPage))}><RiArrowRightSLine /></Button>
+        <Button onClick={() => setIngredientPage(lastPage)}><RiArrowRightSLine /><RiArrowRightSLine /></Button>
+      </div>
+      {filteredIngredients.slice((ingredientPage - 1) * resultsPerPage, ingredientPage * resultsPerPage).map(ingredient => <IngredientCard key={ingredient.name} ingredient={ingredient} />)}
       <FloatingButton handleClickEvent={handleNewIngredient}><RiAddFill /></FloatingButton>
     </IngredientsListPageStyles>
   );
