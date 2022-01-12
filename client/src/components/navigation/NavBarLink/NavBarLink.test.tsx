0f83f56aa6b85ba62d7bb0bb5cbc007a6df168e2
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-01-12 17:21:08 -0500

    Add tests to NavBarLink component

--- client/src/components/navigation/NavBarLink/NavBarLink.test.tsx
+++ client/src/components/navigation/NavBarLink/NavBarLink.test.tsx
@@ -1,3 +1,31 @@
+import { screen } from '@testing-library/react';
 import React from 'react';
+import { renderWrappedComponent } from '../../../utils/helpers/tests.helpers';
+import NavBarLink from './NavBarLink';
 
-test.todo('Write tests for NavBarLink');
+describe('render NavBarLink component', () => {
+  const paths = ['/', '/recipes', '/ingredients', '/recipes/1'];
+  renderWrappedComponent(
+    <div>
+      {paths.map(path => <NavBarLink path={path} key={path}>Link</NavBarLink>)}
+    </div>
+  );
+
+  const links = screen.getAllByRole('link');
+
+  test('correct number of links rendered', () => {
+    expect(links.length).toBe(paths.length);
+  });
+
+  test.todo('verify that links are visible');
+
+  links.forEach((link, index) => {
+    test('component is visible', () => {
+      expect(link).toHaveTextContent('Link');
+    });
+  
+    test('href property is assigned', () => {
+      expect(link).toHaveAttribute('href', paths[index]);
+    });
+  });
+});
--- client/src/components/navigation/NavBarLink/NavBarLink.tsx
+++ client/src/components/navigation/NavBarLink/NavBarLink.tsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import NavBarLinkStyles, { StyledProps } from './NavBarLink.styles';
 
 interface NavBarLinkProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode,
-  path: string,
-  styledProps?: StyledProps
-};
+  children: React.ReactNode;
+  path: string;
+  styledProps?: StyledProps;
+}
 
 function NavBarLink({ children, path, styledProps, ...intrinsic }: NavBarLinkProps) {
   return (
     <Link to={path}>
-      <NavBarLinkStyles role='link' {...styledProps} {...intrinsic}>
+      <NavBarLinkStyles {...styledProps} {...intrinsic}>
         {children}
       </NavBarLinkStyles>
     </Link>
