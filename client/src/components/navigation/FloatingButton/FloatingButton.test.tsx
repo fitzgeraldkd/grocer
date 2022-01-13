0ce35bf4953ccd75a3454c37719e9d320e3c2956
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-01-12 22:02:59 -0500

    Add tests for FloatingButton component

--- client/src/components/navigation/FloatingButton/FloatingButton.test.tsx
+++ client/src/components/navigation/FloatingButton/FloatingButton.test.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWrappedComponent } from '../../../utils/helpers/tests.helpers';
 import FloatingButton from './FloatingButton';
 
-describe('render FloatingButton component', () => {
-  renderWrappedComponent(<FloatingButton>Button</FloatingButton>);
+const renderFloatingButton = (contents: string, callback = () => {}) => {
+  renderWrappedComponent(<FloatingButton onClick={callback}>{contents}</FloatingButton>);
+};
 
+describe('render FloatingButton component', () => {
   test('button is visible', () => {
-    expect(screen.getByRole('button')).toBeVisible();
-    expect(screen.getByRole('button')).toHaveTextContent('Button');
+    renderFloatingButton('Button');
+    const button = screen.getByRole('button');
+    expect(button).toBeVisible();
+    expect(button).toHaveTextContent('Button');
+  });
+
+  test('button can be clicked', () => {
+    const mockCallback = jest.fn();
+    renderFloatingButton('Button', mockCallback);
+    const button = screen.getByRole('button');
+    expect(mockCallback.mock.calls.length).toBe(0);
+    fireEvent.click(button);
+    expect(mockCallback.mock.calls.length).toBe(1);
   });
 });
