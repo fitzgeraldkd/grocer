35265f867208516f959e636c28d6407050aa0bc1
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-01-12 22:07:19 -0500

    Add tests to Button component

--- client/src/components/forms/Button/Button.test.tsx
+++ client/src/components/forms/Button/Button.test.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import { renderWrappedComponent } from '../../../utils/helpers/tests.helpers';
+import Button from './Button';
 
-test.todo('Write tests for Button');
+const renderButton = (contents: string, callback = () => {}) => {
+  renderWrappedComponent(<Button onClick={callback}>{contents}</Button>);
+};
+
+describe('render Button component', () => {
+  test('button is visible', () => {
+    renderButton('Button');
+    const button = screen.getByRole('button');
+    expect(button).toBeVisible();
+    expect(button).toHaveTextContent('Button');
+  });
+
+  test('button can be clicked', () => {
+    const mockCallback = jest.fn();
+    renderButton('Button', mockCallback);
+    const button = screen.getByRole('button');
+    expect(mockCallback.mock.calls.length).toBe(0);
+    fireEvent.click(button);
+    expect(mockCallback.mock.calls.length).toBe(1);
+  });
+});
